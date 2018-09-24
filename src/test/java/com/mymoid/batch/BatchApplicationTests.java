7e998569d68866b3ad3245b5c2bac8d8e0b4212c
cristianprofile <cristianprofile@gmail.com>
2018-09-24 15:48:04 +0200

    Deleted Test Service Config. Spring boot test will detect Spring boot application class Fixed naming test package

--- src/test/java/com/mymoid/batch/BatchApplicationTests.java
+++ src/test/java/com/mymoid/batch/BatchApplicationTests.java
@@ -1,14 +1,22 @@
 package com.mymoid.batch;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.test.AssertFile;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.test.context.junit4.SpringRunner;
 
-//@RunWith(SpringRunner.class)
-//@ContextConfiguration(classes={BatchTestConfig.class, BatchConfiguration.class})
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class BatchApplicationTests {
 
 
-	@Autowired
+   @Autowired
 	private JobLauncherTestUtils jobLauncherTestUtils;
 
 
@@ -18,19 +26,16 @@ public class BatchApplicationTests {
     private static final String EXPECTED_FILE2 = "src/test/resources/users2.csv";
     private static final String OUTPUT_FILE2 = "src/main/resources//users2.csv";
 
-//	@Test
+	@Test
 	public void contextLoads()  throws Exception{
-
 //		JobExecution jobExecution = jobLauncherTestUtils.launchJob();
-//        JobExecution jobExecution = jobLauncherTestUtils.launchStep("step1");
-//		Assert.assertEquals("COMPLETED", jobExecution.getExitStatus().getExitCode());
-//
-//
-//       AssertFile.assertFileEquals(new FileSystemResource(EXPECTED_FILE),
-//                new FileSystemResource(OUTPUT_FILE));
-//
-//        AssertFile.assertFileEquals(new FileSystemResource(EXPECTED_FILE2),
-//                new FileSystemResource(OUTPUT_FILE2));
+        JobExecution jobExecution = jobLauncherTestUtils.launchStep("step1");
+		Assert.assertEquals("COMPLETED", jobExecution.getExitStatus().getExitCode());
+       AssertFile.assertFileEquals(new FileSystemResource(EXPECTED_FILE),
+                new FileSystemResource(OUTPUT_FILE));
+
+        AssertFile.assertFileEquals(new FileSystemResource(EXPECTED_FILE2),
+                new FileSystemResource(OUTPUT_FILE2));
 
 	}
 
