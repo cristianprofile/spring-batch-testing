724e3c6abd7c7861a402ca01d6db0eaf25375356
cristianprofile <cristianprofile@gmail.com>
2018-10-01 13:53:31 +0200

    Added getter and setter application.properties

--- src/main/java/com/mymoid/batch/configuration/FileProperties.java
+++ src/main/java/com/mymoid/batch/configuration/FileProperties.java
@@ -23,15 +23,31 @@ public class FileProperties {
         return server;
     }
 
+    public void setServer(String server) {
+        this.server = server;
+    }
+
     public int getPort() {
         return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
     }
 
     public String getUser() {
         return user;
     }
 
+    public void setUser(String user) {
+        this.user = user;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
